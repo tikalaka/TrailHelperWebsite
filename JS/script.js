d509efb1b64e64232e139461b32cc697a371b65e
zoealinn <47342624+zoealinn@users.noreply.github.com>
2020-02-06 11:27:30 -0700

    moved Dan's JS to the JS file

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+    var x = document.getElementById("demo");
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+      } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    
+    function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+    }
+    
+    function showError(error) {
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          x.innerHTML = "User denied the request for Geolocation."
+          break;
+        case error.POSITION_UNAVAILABLE:
+          x.innerHTML = "Location information is unavailable."
+          break;
+        case error.TIMEOUT:
+          x.innerHTML = "The request to get user location timed out."
+          break;
+        case error.UNKNOWN_ERROR:
+          x.innerHTML = "An unknown error occurred."
+          break;
+      }
+    }
+
+
+
     let key = "b6be47e08eafafc2aa026f941d71b5ea836c6defdb410df07668da19dbc1ee37"
     
     //"description" : returns a description of the trail (may be empty)
