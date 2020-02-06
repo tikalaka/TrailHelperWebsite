f66c8b939f5c145391c0a9546779c63ccfc4f9fd
zoealinn <47342624+zoealinn@users.noreply.github.com>
2020-02-06 15:43:25 -0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,86 +1,105 @@
-$(document).ready(function(){
-    var x = document.getElementById("demo");
+// var x = document.getElementById("demo");
 
-    function getLocation() {
-      if (navigator.geolocation) {
+function getLocation() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
-      } else { 
+    } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
-      }
     }
+}
     
-    function showPosition(position) {
-      x.innerHTML = "Latitude: " + position.coords.latitude + 
-      "<br>Longitude: " + position.coords.longitude;
-    }
+function showPosition(position) {
+    let distance = "10" //grab this from whever the distance is entered
+    getTrails(String(position.coords.latitude), String(position.coords.longitude), distance)
+    getTrailByID(7017456)
+    getTrailConditions(7017456)
+    // x.innerHTML = "Latitude: " + position.coords.latitude + 
+    // "<br>Longitude: " + position.coords.longitude;
+}
     
-    function showError(error) {
-      switch(error.code) {
+function showError(error) {
+    switch(error.code) {
         case error.PERMISSION_DENIED:
-          x.innerHTML = "User denied the request for Geolocation."
-          break;
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
         case error.POSITION_UNAVAILABLE:
-          x.innerHTML = "Location information is unavailable."
-          break;
+            x.innerHTML = "Location information is unavailable."
+            break;
         case error.TIMEOUT:
-          x.innerHTML = "The request to get user location timed out."
-          break;
+            x.innerHTML = "The request to get user location timed out."
+            break;
         case error.UNKNOWN_ERROR:
-          x.innerHTML = "An unknown error occurred."
-          break;
-      }
+            x.innerHTML = "An unknown error occurred."
+            break;
     }
+}
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const trailKey = "200681455-ed23a70461e56c7a6b59a26fbd4c00ba"
 
+//RETURNS AN ARRAY OF TRAILS:
 
-    let key = "b6be47e08eafafc2aa026f941d71b5ea836c6defdb410df07668da19dbc1ee37"
-    
-    //"description" : returns a description of the trail (may be empty)
-    //"name" : name of trailhead
-    //"id" : trailhead id
-    //"park_agency_name" : name of the park agency (NOT the park)
-    //"park_agency_website" : website to the park agency (contact info)
-    //"park_name" : the name of the park
-    //"trip_ids" : an array of trip ids associated with the trailhead
-    function getTrailheads(latitude, longitude, distance){
-        let url = "https://api.transitandtrails.org/api/v1/trailheads.xml?key=" + key
-        + "&distance=" + distance + "&latitude=" + latitude + "&longitude=" + longitude
+// id: 7017456
+// name: "The Living Room"
+// type: "Hike"
+// summary: "A short, moderately steep hike up Red Butte with sandstone slab furniture and great views of the Salt Lake Valley."
+// difficulty: "blue"
+// stars: 4.3
+// starVotes: 22
+// location: "Salt Lake City, Utah"
+// url: "https://www.hikingproject.com/trail/7017456/the-living-room"
+// imgSqSmall: "https://cdn-files.apstatic.com/hike/7037954_sqsmall_1555087147.jpg"
+// imgSmall: "https://cdn-files.apstatic.com/hike/7037954_small_1555087147.jpg"
+// imgSmallMed: "https://cdn-files.apstatic.com/hike/7037954_smallMed_1555087147.jpg"
+// imgMedium: "https://cdn-files.apstatic.com/hike/7037954_medium_1555087147.jpg"
+// length: 2.4
+// ascent: 960
+// descent: -959
+// high: 5977
+// low: 5017
+// longitude: -111.8214
+// latitude: 40.7594
+// conditionStatus: "Unknown"
+// conditionDetails: null
+// conditionDate: "1970-01-01 00:00:00"
+function getTrails(latitude, longitude, distance){
+    let url = "https://www.hikingproject.com/data/get-trails?key=" + trailKey
+        + "&maxDistance=" + distance + "&lat=" + latitude + "&lon=" + longitude
 
-    }
+    fetch(proxyurl + url, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error) => console.log(error))
+}
 
-    //this returns the url to the map
-        //"url" returns the url
-    function getTrailheadMaps(trailheadID){
-        let url = "https://api.transitandtrails.org/api/v1/trailheads/" + trailheadID + "/maps.xml?key=" + key
-    }
+// function getTrailByID(ID){
+//     let url = "https://www.hikingproject.com/data/get-trails-by-id?key=" + trailKey
+//         + "&ids" + ID
 
-    //not all trails return images
-        //"square" returns a square image
-        //"medium" returns a medium image
-    function getTrailheadImages(trailheadID){
-        let url = "https://api.transitandtrails.org/api/v1/trailheads/" + trailheadID + "/photos.xml?key=" + key
-    }
+//     fetch(proxyurl + url, {
+//         method: 'GET'
+//     })
+//     .then(response => response.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
+//     .catch((error) => console.log(error))
+// }
 
-    //tripID will come from the trails object
-        //"trip_ids" from the trailhead object returns an array of all 
-            //trip ids ( 0 - many) -- the tripID parameter is that
-        
-        //"id" : trip id
-        //"alerts" : i would assume warnings / dangers / alerts?
-        //"description" : the description of the trip
-        //"duration" : length of trip
-        //"ending_trailhead_id" : the trailhead id at the end of the trip
-        //"intensity" : dififculty
-        //"length_miles" : the length, in miles
-        //"name" : name of trip
-        //"starting_trailhead_id" : the id of the starting trailhead
-    function getTrip(tripID){
-        let url = "https://api.transitandtrails.org/api/v1/trips/" + tripID + ".xml?key=" + key
-    }
+// function getTrailConditions(ID){
+//     let url = "https://www.hikingproject.com/data/get-conditions?key=" + trailKey
+//         + "&ids" + ID
 
-    //this returns a bunch of coordinates, honestly idk if it's useful or not?
-        //"route" : array of coordinates
-    function getTripRoute(tripID){
-        let url = "https://api.transitandtrails.org/api/v1/trips/" + tripID + "/route.xml?key=" + key
-    }
-})
+//     fetch(proxyurl + url, {
+//         method: 'GET'
+//     })
+//     .then(response => response.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
+//     .catch((error) => console.log(error))
+// }
