1f2eb79a4b33e20b6072cca47d9dd13f03faad49
zoealinn <47342624+zoealinn@users.noreply.github.com>
2020-02-19 14:08:41 -0700

    Buttons function better

--- JS/script.js
+++ JS/script.js
@@ -10,25 +10,29 @@ function getLocation() {
 }
     
 function showPosition(position) {
+    document.getElementById("latitude").value = position.coords.latitude
+    document.getElementById("longitude").value = position.coords.longitude
+    getWeather(String(position.coords.latitude), String(position.coords.longitude))
+}
+
+function searchForTrails(){
     var distance = document.getElementById("distance").value;
     var minRating;
     var elements2 = document.getElementsByName("minimumRating");              
-        for(i = 0; i < elements2.length; i++) { 
-            if(elements2[i].checked) 
-            minRating = "" + elements2[i].value; 
-        }
+    for(i = 0; i < elements2.length; i++) { 
+        if(elements2[i].checked) 
+        minRating = "" + elements2[i].value; 
+    }
     var length = document.getElementById("length").value;
     var difficulty;
     var elements3 = document.getElementsByName("difficulty");
-        for(i = 0; i < elements3.length; i++){
-            if(elements3[i].checked){
-                difficulty = "" + difficultyDecider(elements3[i].value)
-            }
+    for(i = 0; i < elements3.length; i++){
+        if(elements3[i].checked){
+            difficulty = "" + difficultyDecider(elements3[i].value)
         }
-    document.getElementById("latitude").value = position.coords.latitude
-    document.getElementById("longitude").value = position.coords.longitude
-    getTrails(String(position.coords.latitude), String(position.coords.longitude), String(distance), String(minRating), String(length), String(difficulty))
-    getWeather(String(position.coords.latitude), String(position.coords.longitude))
+    }
+
+    getTrails(String(document.getElementById("latitude").value), String(document.getElementById("longitude").value), String(distance), String(minRating), String(length), String(difficulty))
 }
     
 function showError(error) {
@@ -79,7 +83,8 @@ const trailKey = "200681455-ed23a70461e56c7a6b59a26fbd4c00ba"
 
 function getTrails(latitude, longitude, distance, minStars, length, difficulty){
     let url = "https://www.hikingproject.com/data/get-trails?key=" + trailKey
-        + "&maxDistance=" + distance + "&lat=" + latitude + "&lon=" + longitude + "&minStars=" + minStars // + "&maxResults=15"            
+        + "&maxDistance=" + distance + "&lat=" + latitude + "&lon=" + longitude 
+        + "&minStars=" + minStars  + "&maxResults=500"            
 
     fetch(proxyurl + url, {
         method: 'GET'
